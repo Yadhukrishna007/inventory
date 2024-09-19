71705ab3ad942b6a1152e6c2980f7e70f310419d
yadhukrishna1322 <yadhu@royalclass.group>
2024-09-19 17:23:13 +0400

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="bg-green-400">page</div>;
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { lusitana } from "@/app/ui/font";
+import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -11,7 +12,10 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong className={`${lusitana.className} font-[200px]`}>
+              Welcome to Acme.
+            </strong>{" "}
+            This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -26,6 +30,16 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <div className="relative w-[560px] h-[620px] md:w-[1000px] md:h-[760px]  ">
+            <Image
+              src="/hero-desktop.png"
+              fill
+              className="object-cover"
+              sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+              alt="Screenshots of the dashboard project showing desktop version"
+              priority={false}
+            />
+          </div>
         </div>
       </div>
     </main>
